f101fc6ff4dfa0721bcfd68c3b5d1ee22039f3c4
Chanif <chanifg19@gmail.com>
2025-04-24 08:39:30 +0700

    Add files via upload

---
+++ responsibd.sql
@@ -0,0 +1,178 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2025 at 07:52 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `responsibd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` varchar(10) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jurusan` varchar(20) DEFAULT 'Umum',
+  `tgl_daftar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nama`, `jurusan`, `tgl_daftar`) VALUES
+('001', 'Andi Prayoga', 'Teknologi Informasi', '2024-01-10'),
+('002', 'Siti Maimunah', 'Manajemen', '2024-02-05'),
+('003', 'Anwar Joko', 'Umum', '2024-03-20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` varchar(10) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `penerbit` varchar(50) DEFAULT NULL,
+  `tahun_terbit` int(11) DEFAULT NULL CHECK (`tahun_terbit` between 1900 and 2025),
+  `id_penerbit` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul`, `penerbit`, `tahun_terbit`, `id_penerbit`) VALUES
+('B001', 'Basis Data Dasar', 'Gramedia', 2022, 'P001'),
+('B002', 'Pemrograman Python', 'Erlangga', 2021, 'P002'),
+('B003', 'Sistem Operasi', 'Andi', 2019, 'P003'),
+('B004', 'Jaringan Komputer', 'Gramedia', 2020, 'P001'),
+('B005', 'Kecerdasan Buatan', 'Elex Media', 2023, 'P004');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(11) NOT NULL,
+  `id_anggota` varchar(10) DEFAULT NULL,
+  `id_buku` varchar(10) DEFAULT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date DEFAULT NULL,
+  `Denda` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `id_anggota`, `id_buku`, `tgl_pinjam`, `tgl_kembali`, `Denda`) VALUES
+(7, '001', 'B001', '2025-04-01', '2025-04-10', 6000),
+(8, '002', 'B003', '2025-04-05', '2025-04-15', 0),
+(9, '003', 'B002', '2025-04-10', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penerbit`
+--
+
+CREATE TABLE `penerbit` (
+  `id_penerbit` varchar(10) NOT NULL,
+  `nama_penerbit` varchar(50) DEFAULT NULL,
+  `alamat` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penerbit`
+--
+
+INSERT INTO `penerbit` (`id_penerbit`, `nama_penerbit`, `alamat`) VALUES
+('P001', 'Gramedia', 'Jakarta'),
+('P002', 'Erlangga', 'Bandung'),
+('P003', 'Andi', 'Yogyakarta'),
+('P004', 'Elex Media', 'Jakarta');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD KEY `fk_penerbit` (`id_penerbit`);
+
+--
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD KEY `id_anggota` (`id_anggota`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indexes for table `penerbit`
+--
+ALTER TABLE `penerbit`
+  ADD PRIMARY KEY (`id_penerbit`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `buku`
+--
+ALTER TABLE `buku`
+  ADD CONSTRAINT `fk_penerbit` FOREIGN KEY (`id_penerbit`) REFERENCES `penerbit` (`id_penerbit`);
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
