9d54e717208a888c293aaffb1d29f8bd1dae2552
Chanif <chanifg19@gmail.com>
2025-06-26 00:27:42 +0700

    Add files via upload

---
+++ universitas.sql
@@ -0,0 +1,271 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 25 Jun 2025 pada 19.27
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `universitas`
+--
+
+DELIMITER $$
+--
+-- Prosedur
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tambah_transaksi` (IN `p_id_pelanggan` INT, IN `p_id_buku` INT, IN `p_jumlah` INT)   BEGIN
+    DECLARE harga_buku DECIMAL(10,2);
+    DECLARE total_harga DECIMAL(10,2);
+    DECLARE stok_saat_ini INT;
+
+    -- Cek stok
+    SELECT stok, harga INTO stok_saat_ini, harga_buku
+    FROM buku
+    WHERE id_buku = p_id_buku;
+
+    IF stok_saat_ini < p_jumlah THEN
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'Stok tidak mencukupi!';
+    ELSE
+        SET total_harga = harga_buku * p_jumlah;
+
+        -- Kurangi stok
+        UPDATE buku
+        SET stok = stok - p_jumlah
+        WHERE id_buku = p_id_buku;
+
+        -- Tambahkan transaksi
+        INSERT INTO transaksi(id_pelanggan, id_buku, jumlah, total_harga, tanggal_transaksi)
+        VALUES (p_id_pelanggan, p_id_buku, p_jumlah, total_harga, CURDATE());
+
+        -- Tambahkan total belanja ke pelanggan
+        UPDATE pelanggan
+        SET total_belanja = total_belanja + total_harga
+        WHERE id_pelanggan = p_id_pelanggan;
+    END IF;
+END$$
+
+--
+-- Fungsi
+--
+CREATE DEFINER=`root`@`localhost` FUNCTION `hitung_diskon` (`total_belanja` DECIMAL(10,2)) RETURNS DECIMAL(5,2) DETERMINISTIC BEGIN
+    DECLARE diskon DECIMAL(5,2);
+
+    IF total_belanja < 1000000 THEN
+        SET diskon = 0.00;
+    ELSEIF total_belanja >= 1000000 AND total_belanja <= 5000000 THEN
+        SET diskon = 0.05;
+    ELSE
+        SET diskon = 0.10;
+    END IF;
+
+    RETURN diskon;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_brg` varchar(10) NOT NULL,
+  `nama_brg` varchar(50) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_brg`, `nama_brg`, `stok`) VALUES
+('A11', 'Keyboard', 15),
+('A12', 'DVD R-W', 10);
+
+--
+-- Trigger `barang`
+--
+DELIMITER $$
+CREATE TRIGGER `deleteChild` AFTER DELETE ON `barang` FOR EACH ROW DELETE FROM pembelian
+WHERE id_brg = OLD.id_brg
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul` varchar(100) DEFAULT NULL,
+  `penulis` varchar(100) DEFAULT NULL,
+  `harga` decimal(10,2) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `total_belanja` decimal(10,2) DEFAULT 0.00,
+  `status_member` enum('REGULER','GOLD','PLATINUM') DEFAULT 'REGULER'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Trigger `pelanggan`
+--
+DELIMITER $$
+CREATE TRIGGER `UbahStatus` BEFORE UPDATE ON `pelanggan` FOR EACH ROW IF NEW.total_belanja >= 10000000 THEN
+    SET NEW.status_member = 'PLATINUM';
+ELSEIF NEW.total_belanja >= 5000000 THEN
+    SET NEW.status_member = 'GOLD';
+ELSE
+    SET NEW.status_member = 'REGULER';
+END IF
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id_pem` int(11) NOT NULL,
+  `id_brg` varchar(10) DEFAULT NULL,
+  `jml_beli` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Trigger `pembelian`
+--
+DELIMITER $$
+CREATE TRIGGER `updateStok` AFTER INSERT ON `pembelian` FOR EACH ROW UPDATE barang
+SET stok = stok + NEW.jml_beli
+WHERE id_brg = NEW.id_brg
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `updateStokEdit` AFTER UPDATE ON `pembelian` FOR EACH ROW UPDATE barang
+SET stok = stok + (NEW.jml_beli - OLD.jml_beli)
+WHERE id_brg = NEW.id_brg
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_pelanggan` int(11) DEFAULT NULL,
+  `id_buku` int(11) DEFAULT NULL,
+  `jumlah` int(11) DEFAULT NULL,
+  `total_harga` decimal(10,2) DEFAULT NULL,
+  `tanggal_transaksi` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_brg`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indeks untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id_pem`),
+  ADD KEY `id_brg` (`id_brg`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_brg`) REFERENCES `barang` (`id_brg`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
